89785cb0db2b533acb1c8fac26307361658b226f
Deepak3848 <41951786+Deepak3848@users.noreply.github.com>
2024-10-12 09:43:49 +0000

    conversion of datatypes

--- Basics/test.js
+++ Basics/test.js
@@ -1,1 +1,20 @@
-console.log('Deepak');
+//console.log('Deepak');
+
+//conversion & operarions -
+let score = true;
+let valueInNumber = Number(score);
+console.log('valueInNumber -', valueInNumber);
+
+// 33 => 33 '33abd' => NaN
+// true => 1 & false => 0
+// null => 0 undefined => NaN 
+
+let isLoggedIn = 'dwdw';
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log('booleanIsLoggedIn - ', booleanIsLoggedIn);
+
+/*
+ Boolean conversion -
+ 0 => false 1 => true
+ '' => false 'something' => true;
+ */
